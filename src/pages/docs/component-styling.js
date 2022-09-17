66edd0660b1761a0d606359dee5a61ef684eef19
Koregano73 <ctcwong73@gmail.com>
2022-09-16 20:35:20 -0700

    increased sidebar min height

--- src/pages/docs/component-styling.js
+++ src/pages/docs/component-styling.js
@@ -2,7 +2,7 @@ import React from "react";
 import { DocsLayout } from "../../layouts";
 import "../../styles/docs.css";
 import Codesnippet from "../../assets/doc-data/Codesnippet.jpg"
-
+import styled from "styled-components";
 export default function ComponentStyling() {
   return (
     <DocsLayout>
@@ -10,6 +10,12 @@ export default function ComponentStyling() {
         <h2>Component Styling</h2>
         <p>After adding a component class name, under the component details tab, you can access details about the active/selected component - including a code snippet (live generated boilerplate code), the HTML elements, state, actions or props in the component. The code snippet dynamically renders grid area position, color, and size of the component. Additionally OverVue can now make use of the popular Vue component Library Element Plus.</p>
         <img className="gif" alt="codesnippet" src={Codesnippet}></img>
+        <div className="desc">
+          <div>
+            <p>After adding a component class name, under the component details tab, you can access details about the active/selected component - including a code snippet (live generated boilerplate code), the HTML elements, state, actions or props in the component. The code snippet dynamically renders grid area position, color, and size of the component. Additionally OverVue can now make use of the popular Vue component Library Element Plus.</p>
+          </div>
+          <img className="gif" alt="codesnippet" src={Codesnippet}></img>
+        </div>
         {/* <h3>Positioning and Snap to Grid</h3>
         <h3>Color Updates</h3>
         <h3>Size Updates</h3>
