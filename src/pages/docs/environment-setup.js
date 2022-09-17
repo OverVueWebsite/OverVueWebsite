8dc8a001e25a6468c51e389890fd9cfafcbd2625
Koregano73 <ctcwong73@gmail.com>
2022-09-16 18:12:18 -0700

    set up grid doc

--- src/pages/docs/environment-setup.js
+++ src/pages/docs/environment-setup.js
@@ -6,11 +6,11 @@ export default function EnvironmentSetup() {
   return (
     <DocsLayout>
       <div className="docs__content">
-        <h2>Setting Up Your OverVue Environment</h2>​
+        <h2>Setting Up Your OverVue Environment</h2>
+        ​<h3>Setting Grid Density</h3>
+        <p>A key feature of OverVue is the ability to dynamically change the grid layout of the prototyping canvas before you start. Within the navigation bar on the top of the application, there are two buttons: Height and Width. Click either to have the a pulldown menu show what how many grid lines you want in each axis.</p>
+        <img className="shadow" alt="show grid density" src="./gridDensity.gif"></img>
         <ul>
-          <li>
-            Setting grid density
-          </li>
           <li>
             connecting to slack
           </li>
