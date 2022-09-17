73635cac845debd97c5581c6f93eb2d63cc55596
Alex Law <alexlaw528@gmail.com>
2022-09-17 14:05:03 -0700

    Update Navbar to fetch Github stars during client-side render

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "gatsby";
 import logo from "../../assets/logo/overvue-nav.png";
 import "./navbar.css";
  
-
 // OG Nav Bar
 export default function Navbar() {
+
+  // Fetch Github stars at runtime
+  const [starsCount, setStarsCount] = useState('')
+  useEffect(() => {
+    // Fetch data from Github api
+    fetch('https://api.github.com/repos/open-source-labs/OverVue')
+      .then(response => response.json())
+      .then(data => {
+        const starsRounded = (data.stargazers_count/1000).toFixed(1);
+        setStarsCount(starsRounded + 'k');
+      })
+  }, [])
+  
   return (
     <nav className="nav">
       <Link className="nav__logo" to={"/"}>
@@ -21,7 +33,7 @@ export default function Navbar() {
         </Link>
         <a href="https://github.com/open-source-labs/OverVue">
           <img className='github_logo' src='https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png' alt="black github logo"></img>
-          2,294
+          {starsCount}
         </a>
       </div>
     </nav>
