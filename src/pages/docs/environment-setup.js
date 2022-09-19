594365637bf6219107d8661a7209f98472b8aec7
Koregano73 <ctcwong73@gmail.com>
2022-09-19 14:33:00 -0700

    added more docs for routes and comps

--- src/pages/docs/environment-setup.js
+++ src/pages/docs/environment-setup.js
@@ -23,14 +23,14 @@ export default function EnvironmentSetup() {
             ​<h3>Connect to Slack</h3>
             <p>OverVue offers Slack within the application to link your OverVue instance into any slack channel. After logging in, you can send a message to your Slack channel after saving the project.</p>
           </div>
-          <img className="gif" alt="show grid density" src={SlackOAuth}></img>
+          <img className="gif" alt="show slack" src={SlackOAuth}></img>
         </div>
         <div className="desc">
           <div>
             ​<h3>Add Project TypeScript and OAuth </h3>
             <p>OverVue includes the feature to implement TypeScript and Google/Github OAuth when exporting your project prototype. To turn TypeScript or Oauth modes on or off, click on the Settings gear icon in the right corner of the nav bar and click the toggle at the bottom of the menu of each respective setting.</p>
           </div>
-          <img className="gif" alt="show grid density" src={OptionsPullDown}></img>
+          <img className="gif" alt="show options" src={OptionsPullDown}></img>
         </div>
       </div>
     </DocsLayout>
