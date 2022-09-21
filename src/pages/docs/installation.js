1c28890119a82e94af4a7ff610cd668ba3c813f5
Koregano73 <ctcwong73@gmail.com>
2022-09-21 12:04:00 -0700

    wrote installation documentation

--- src/pages/docs/installation.js
+++ src/pages/docs/installation.js
@@ -8,26 +8,12 @@ export default function Installation() {
       <div className="docs__content">
         <h2>Installation</h2>
         <p>
-          OverVue is compatible with both MacOS and WSL. Quickly install and set up using the quickstart guide for your operating system below.
+          OverVue is compatible with both MacOS and WSL. Quickly install and set up using the quickstart guide for your operating system below. To try out the development version of OverVue, head over to our <a href="https://github.com/open-source-labs/OverVue">Github page</a> on how to get started.
         </p>
-        <h2>Installation Instructions</h2>​
-        <ul>
-          <li>
-            1. Download the desktop App. 
-          </li>
-          <li>
-            2. Install Dependencies
-          </li>
-          <li>
-            3. Run electron app in dev mode (nb. Vue Devtools will launch automatically)
-          </li>
-          <li>
-            4. Run build
-          </li>
-        </ul>
-        <p>
-          {" "}
-        </p>
+        <h3>Installation Instructions</h3>
+        <p>1. Download the desktop App for <a href="">MacOS</a> or <a href="">Windows</a>.</p>
+        <p>2. Extract folder.</p>
+        <p>3. Run OverVue.exe.</p>​
       </div>
     </DocsLayout>
   );
