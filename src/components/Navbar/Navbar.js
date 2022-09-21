d14b78fdb42cdd3e1f0497ebb6d8f2d601c8ab25
Alex Law <alexlaw528@gmail.com>
2022-09-21 16:20:59 -0700

    Add nav-bar responsiveness.

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,18 +2,21 @@ import React, { useState, useEffect } from "react";
 import { Link } from "gatsby";
 import logo from "../../assets/logo/overvue-nav.png";
 import "./navbar.css";
- 
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+
 // OG Nav Bar
 export default function Navbar() {
+  const [showNavLinks, setShowNavLinks] = useState(false);
+  const [starsCount, setStarsCount] = useState('');
 
   // Fetch Github stars at runtime
   const defaultStars = '2.3k'; // Use this number if github fetch fails
 
-  const [starsCount, setStarsCount] = useState('');
   useEffect(() => {
     // Check localStorage for starCount, if present use that starsCount 
     const localStorageStars = JSON.parse(window.localStorage.getItem('starsCount'));
-    if (!localStorageStars) {
+    if (localStorageStars) {
       setStarsCount(localStorageStars + 'k');
     }
     // Retrieve from Github if not present
@@ -21,13 +24,13 @@ export default function Navbar() {
       // Fetch data from Github api
       fetch('https://api.github.com/repos/open-source-labs/OverVue')
         .then(response => {
+          console.log('fetching stars...')
           if (response.status === 403) {
             throw new Error('403 error');
           }
           else return response.json()
         })
         .then(data => {
-          console.log('fetching stars...')
           const starsRounded = (data.stargazers_count/1000).toFixed(1);
           window.localStorage.setItem('starsCount', starsRounded);
           setStarsCount(starsRounded + 'k');
@@ -44,7 +47,7 @@ export default function Navbar() {
       <Link className="nav_logo" to={"/"}>
         <img class="overVue_logo" src={logo} alt={"logo"} />
       </Link>
-      <div className="nav_actions">
+      <div className="nav_actions" id={showNavLinks ? "hidden" : ""}>
         <Link className="nav_docs" to={"/docs"}>
           Docs
         </Link>
@@ -56,6 +59,9 @@ export default function Navbar() {
           {starsCount}
         </a>
       </div>
+      <IconButton className="ham-btn" onClick={()=>setShowNavLinks(!showNavLinks)}>
+        <MenuIcon/>
+      </IconButton>
     </nav>
   );
 }
